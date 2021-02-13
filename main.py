6c13231a42d96cb67ea06bb92a00946a5620217a
katarzynaadamczyk <kasia.cholost@gmail.com>
2021-02-13 17:47:18 +0100

    Added comments, working well

--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 # character is firstly changed to lowercase and then added / checked in the dictionary
 
 def are_anagrams(s1, s2, s3):
-    # check if the size of each string is equal
-    if len(s1) == len(s2) and len(s1) == len(s3):
+    # check if the size of each string is equal 
+    # and the strings are different
+    if len(s1) == len(s2) and len(s1) == len(s3) and not (s1 == s2 or s1 == s3 or s2 == s3):
         # if so create a list of dictionaries for each string
         # each dictionary will contain letters with their corresponding quantity in given text
         dicts = list()
@@ -43,6 +44,7 @@ def main():
     print("Result for 'kasia', 'asiak', 'askia' is " + str(are_anagrams('kasia', 'asiak', 'askia')))
     print("Result for 'kasia', 'asiak', 'asial' is " + str(are_anagrams('kasia', 'asiak', 'asial')))
     print("Result for 'kasia', 'asiak', 'asmia' is " + str(are_anagrams('kasia', 'asiak', 'asmia')))
+    print("Result for 'kasia', 'asiak', 'kasia' is " + str(are_anagrams('kasia', 'asiak', 'kasia')))
 
 if __name__ == "__main__":
     main()
